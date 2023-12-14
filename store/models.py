e54e172e58f123c17b4766f4ceeee4bb679302c2
Ali Alaql <23135660+alikhleel@users.noreply.github.com>
2023-12-14 18:10:14 +0300

    implement models for store app

--- store/models.py
+++ store/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class Customer(models.Model):
+    customer_id = models.IntegerField()
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+
+
+class Order(models.Model):
+    order_id = models.IntegerField()
+    quantity = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    reviewer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    review = models.CharField(max_length=1000)
