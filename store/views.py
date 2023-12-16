6f25ef237f76e2c195b15c4c00ed5fa0872dd419
Ali Alaql <23135660+alikhleel@users.noreply.github.com>
2023-12-16 11:30:35 +0300

    implement routers and views

--- store/views.py
+++ store/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from store.models import Product, Customer, Review, Order
+from store.serializers import ProductSerializer, CustomerSerializer, ReviewSerializer, OrderSerializer
+
+
+class ProductViewSets(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CustomerViewSets(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class OrderViewSets(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class ReviewViewSets(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
